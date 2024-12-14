97ca744be96696ff83a040842faed59a56cc3e35
Nini Mtchedlishvili <145496919+mtchedlishvilinini@users.noreply.github.com>
2024-12-14 15:29:33 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './Board/Board';
+
+
+function App() {
+  return (
+    <div className='flex'>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
